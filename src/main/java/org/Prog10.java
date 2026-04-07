1b2d41bf8ede40ef2d7e2ff77349cfb1e3f9a869
alekmenski <alekmenski@yandex.ru>
2026-04-07 14:20:10 +0300

    Вычисляет факториал от 1 до 10

--- src/main/java/org/Prog10.java
+++ src/main/java/org/Prog10.java
@@ -1,18 +1,10 @@
 package org;
-
+// Вычисляет факториал от 1 до 10
 public class Prog10 {
     public static void main(String[] args) {
         int num = 1;
         int factorial = 1;
         while (num <= 10) {
- //           int factorial = 1;
-//            int num1 = 1;
-//            while (num1 <= num) {
-//                factorial = factorial * num1;
-//                num1++;
-//            }
-//            System.out.println(num + "! = " + factorial);
-//            num++;
             factorial = factorial * num;
             System.out.println(num + "! = " + factorial);
             num++;
