f07bcb803435870a38a077bde2d460229055c281
alekmenski <alekmenski@yandex.ru>
2026-04-17 11:40:58 +0300

    Исключить одинаковые элементы массива каждое значение должно присуствывать в единичном экземпляре

--- src/main/java/Array/Array6.java
+++ src/main/java/Array/Array6.java
@@ -7,22 +7,25 @@ public class Array6 {
     public static void main(String[] args) {
         int[] array = {5, 5, 6, 3, 1, 3};
         int[] array2 = new int[array.length];
-        int arrayCount = 0;
-        int arrayCount2 = 0;
+        int index = 0;
         System.out.println(Arrays.toString(array));
+
         for (int i = 0; i < array.length; i++) {
-            array2[i] = array[i];
-        }
-        for (int i = 0; i < array.length; i++) {
-            arrayCount = array[i];
-            for (int j = 0; j < array2.length; j++) {
-                arrayCount2 = array2[j];
-                if (arrayCount != arrayCount2) {
-                    System.out.print( arrayCount + " ");
-                }else {
-                    System.out.print("0" + " ");
+            int value = array[i];
+            boolean contains = false;
+            for (int y = i + 1; y < array.length; y++) {
+                if(value == array[y]) {
+                    contains = true;
+                    break;
                 }
+
+            }
+            if (!contains) {
+                array2[index] = array[i];
+                index++;
             }
         }
+        int[] arrayResult = Arrays.copyOf(array2, index);
+        System.out.println(Arrays.toString(arrayResult));
     }
 }
